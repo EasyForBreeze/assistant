ae7a91da7c2f21889362eb4516fc0f44061b3a07
Breams <breams.jd@gmail.com>
2025-09-17 15:38:38 +0300

    bas1

--- obj/Debug/net9.0/Assistant.AssemblyInfo.cs
+++ obj/Debug/net9.0/Assistant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assistant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c62e1858312e048fae1524c291b95b5cc229843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6f248f62a8d49fc9d2b7b4a39b1f1b7a483e3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assistant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assistant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
